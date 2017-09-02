def748025b7a9a876beff023d63fd7399381d6a9
munaf <munafaqeelmahdi@gmail.com>
2017-09-02 17:49:37 +0300

    added website

---
+++ javascript/javascript.js
@@ -0,0 +1,18 @@
+function dropdownmenu() {
+    document.getElementById("dropdownmenu").style.display = 'block';
+}
+function dropdownmenu_hide() {
+    document.getElementById("dropdownmenu").style.display = 'none';
+}
+function about_dialog_show() {
+    document.getElementById("about_dialog_bg").style.display = 'block';
+}
+function about_dialog_hide() {
+    document.getElementById("about_dialog_bg").style.display = 'none';
+}
+function btn_title_show(title) {
+    document.getElementById('btn_title').innerHTML = title;
+}
+function btn_title_hide() {
+    document.getElementById('btn_title').innerHTML = '';
+}
